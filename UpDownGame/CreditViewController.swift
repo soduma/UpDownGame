6e4ee474247519a7aa54b6141eeb2a19dacab4c8
soduma <soduma@naver.com>
2020-09-09 12:31:02 +0900

    first commit

---
+++ UpDownGame/CreditViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CreditViewController.swift
+//  UpDownGame
+//
+//  Created by 장기화 on 2020/08/18.
+//  Copyright © 2020 soduma. All rights reserved.
+//
+
+import UIKit
+
+class CreditViewController: UIViewController {
+    
+    @IBAction func touchUpDissmissButton(_ sender: UIButton) {
+            dismiss(animated: true,
+                    completion: nil)
+    }
+}
